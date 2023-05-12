0158e9df007a7d8ce19dbce74e99dc0e18bbefff
HyungJoon Yoon <hysy9255@gmail.com>
2023-05-12 20:36:43 +0900

    edited frontend file structure

--- board/src/API/API.js
+++
@@ -1,9 +0,0 @@
-const IP = '52.79.250.243';
-const PORT = 9000;
-const URL = `http://${IP}:${PORT}`;
-
-const API = {
-  diary: `${URL}/diary`,
-  uploadImg: `${URL}/diary/image/upload`,
-};
-export default API;
