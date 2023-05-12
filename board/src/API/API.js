03f93125c1b247d7f8b7f66dcb68313bd1af5fb2
Oh9yu <kog6148@gmail.com>
2023-05-12 17:52:11 +0900

    editor 기능

--- board/src/API/API.js
+++ board/src/API/API.js
@@ -1,8 +1,9 @@
-const IP = '10.58.52.155';
+const IP = '10.58.52.75';
 const PORT = 9000;
 const URL = `http://${IP}:${PORT}`;
 
 const API = {
+  createDiary: `${URL}/diary`,
   uploadImg: `${URL}/diary/image/upload`,
 };
 export default API;
--- board/src/pages/Editor/Editor.js
+++ board/src/pages/Editor/Editor.js
@@ -1,60 +1,94 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import API from '../../API/API';
 
 const Editor = () => {
+  const navigate = useNavigate('');
+  const [title, setTitle] = useState('');
   const [contents, setContents] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
   const quillRef = useRef(null);
 
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        ['link', 'image'],
-        ['clean'],
-      ],
-      handlers: {
-        image: () => {
-          const input = document.createElement('input');
-          input.setAttribute('type', 'file');
-          input.setAttribute('accept', 'image/*');
-          input.click();
-
-          input.onchange = () => {
-            const file = input.files[0];
-            const formData = new FormData();
-            formData.append('file', file);
-
-            fetch(`${API.uploadImg}`, {
-              method: 'POST',
-              body: formData,
-            })
-              .then(res => res.json())
-              .then(data => {
-                const range = quillRef.current.getEditor().getSelection();
-                const img = data.imageUrl;
-                quillRef.current
-                  .getEditor()
-                  .insertEmbed(range.index, 'image', img);
-              });
-          };
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, false] }],
+          ['bold', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['image'],
+        ],
+        handlers: {
+          image: () => {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.click();
+            input.onchange = () => {
+              const file = input.files[0];
+              const formData = new FormData();
+              formData.append('file', file);
+              fetch(`${API.uploadImg}`, {
+                method: 'POST',
+                body: formData,
+              })
+                .then(res => res.json())
+                .then(data => {
+                  const img = data.imageUrl;
+                  setContents(
+                    prevContent => `${prevContent}<img src="${img}" alt='img'/>`
+                  );
+                  if (thumbnail.length === 0) {
+                    setThumbnail(img);
+                  }
+                })
+                .catch(err => console.log(err));
+            };
+          },
         },
       },
-    },
-  };
-
-  console.log(contents);
+    };
+  }, []);
 
   const inputHandler = value => {
     setContents(value);
   };
 
+  const titleHandler = e => {
+    setTitle(e.target.value);
+  };
+
+  const createPost = () => {
+    if (!title || !contents) return;
+    fetch(`${API.createDiary}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: title,
+        contents: contents,
+        thumbnail: thumbnail,
+      }),
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.message === 'success') {
+          navigate('/');
+        }
+      });
+  };
+
   return (
     <Container>
+      <Title
+        type="text"
+        value={title}
+        onChange={titleHandler}
+        placeholder="제목을 입력하세요"
+      />
+      <SubmitBtn onClick={createPost}>게시글 등록</SubmitBtn>
       <StyledQuill
         modules={modules}
         value={contents}
@@ -67,12 +101,31 @@ const Editor = () => {
 
 export default Editor;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 480px;
+  margin: auto;
+`;
+
+const Title = styled.input`
+  max-width: 600px;
+  padding: 10px;
+`;
 
 const StyledQuill = styled(ReactQuill)`
   max-width: 800px;
   height: 600px;
+  margin: 5px 0;
   @media screen and (min-width: 200px) and (max-width: 480px) {
-    height: 80vh;
+    height: 70vh;
   }
+  .ql-editor {
+    line-height: 1.5;
+    overflow: scroll;
+  }
+`;
+
+const SubmitBtn = styled.button`
+  height: 30px;
 `;
